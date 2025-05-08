f3b3ad43474ced46f83b00160ffb529ffa298299
LibiaManoela <libiamanoelas.hentz@gmail.com>
2025-05-08 19:01:18 -0300

    evoluindo desafio

--- dio-desafio-banco-poo/src/Main.java
+++ dio-desafio-banco-poo/src/Main.java
@@ -2,11 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente cliente = new Cliente();
+		cliente.setNome("Libia");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
